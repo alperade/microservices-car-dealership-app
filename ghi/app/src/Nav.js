f2a44a4c4ee96b352f9e2df79361ba46b524c2fc
David Leung <david.leung@live.com>
2022-10-26 16:23:02 -0500

    updated nav links with the drop down format

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -11,23 +11,15 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/customers/new">Add a new customer</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/salespersons/new">Add a new salesperson</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/salesrecords/new">Add a new salesrecord</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/salespersons">Sales Persons</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/salesrecords">Sales Records</NavLink>
+             <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Sales</a>
+              <ul className="dropdown-menu">
+                <li><NavLink className="dropdown-item" to="/customers/new">Add a new customer</NavLink></li>
+                <li><NavLink className="dropdown-item" to="/salespersons/new">Add a new salesperson</NavLink></li>
+                <li><NavLink className="dropdown-item" to="/salesrecords/new">Add a new salesrecord</NavLink></li>
+                <li><NavLink className="dropdown-item" to="/salespersons">Sales Person's Sale History</NavLink></li>
+                <li><NavLink className="dropdown-item" to="/salesrecords">Sales Records</NavLink></li>
+              </ul>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Service</a>
